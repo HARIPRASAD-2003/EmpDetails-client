4e8f5667866a9dad0b66e9138646cd33060af160
HARIPRASAD <hari11122003@gmail.com>
2023-12-12 07:50:44 +0530

    first commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,213 @@
+import React,{useEffect, useState} from 'react'
+import './Home.css'
+const Home = () => {
+
+    const baseURL = "http://localhost:8080";
+    const [userData, setUserData] = useState([]);
+    const [create, setCreate] = useState(false);
+    const [updatedEmpname, setUpdatedEmpname] = useState("");
+    const [updatedDept, setUpdatedDept] = useState("");
+    const [updatedDesignation, setUpdatedDesignation] = useState("");
+    const [updatedAddress, setUpdatedAddress] = useState("");
+    const [updatedSalary, setUpdatedSalary] = useState("");
+    const [updatedDob, setUpdatedDob] = useState("");
+    const [delEmpId, setDelEmpId] = useState("");
+
+
+
+    const handleUpdate = () => {
+        
+    }
+    
+    const handleCreate = async() => {
+        try {
+            const response = await fetch(baseURL + '/newEmp', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    name: updatedEmpname,
+                    dept: updatedDept,
+                    des: updatedDesignation,
+                    sal: updatedSalary,
+                    dob: updatedDob,
+                    address: updatedAddress
+
+                })
+            });
+            if(!response.ok){
+                throw new Error("res not OK!!");
+            }
+            fetchData();     
+            handleCancel();       
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const handleDelete = async() => {
+        try{
+            const response = await fetch(baseURL + "/delEmp", {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    id: delEmpId,
+                })
+            });
+            if(!response.ok){
+                throw new Error("res.not OK");
+            }
+            fetchData();
+            handleCancel();
+        } catch(error){
+            console.log(error)
+        }
+    }
+
+
+
+    const handleCancel = () => {
+        setUpdatedAddress("");
+        setUpdatedDept("");
+        setUpdatedDesignation("");
+        setUpdatedDob("");
+        setUpdatedEmpname("");
+        setUpdatedSalary("");
+        setCreate(false);
+    }
+    const fetchData = async() => {
+        try{
+            const response = await fetch(baseURL+'/getEmp');
+            if(!response.ok)
+                throw new Error("res not OK!!!");
+            const data = await response.json();
+            setUserData(data);
+            console.log(data);
+        } catch(Error){
+            console.log(Error);
+        }
+    }
+
+    useEffect(()=>{
+        fetchData();
+    },[])
+
+  return (
+    <div>
+
+        <div className='Main'>
+            <div className='Home'>
+                <h1>Employee Details</h1>
+            </div>
+            <div className='options'>
+                <button onClick={() => setCreate(true)}>Add Employee</button>
+            </div>
+            {create && (
+                <div >
+                <div className='ModalOverlay'></div>
+                <div style={{width: "50%"}} className='Modal'>
+                    <h2>Create Employee</h2>
+                    <div style={{width: "100%"}} className='ModalContent'>
+
+                    <label htmlFor="updatedEmpname">Name:</label>
+                    <input
+                        type='text'
+                        id="updatedEmpname"
+                        value={updatedEmpname}
+                        onChange={(e) => setUpdatedEmpname(e.target.value)}
+                    />
+                    <label htmlFor="updatedDept">Department:</label>
+                    <input
+                        type='text'
+                        id="updatedDept"
+                        value={updatedDept}
+                        onChange={(e) => setUpdatedDept(e.target.value)}
+                    />
+                    <label htmlFor="updatedDesignation">Designation:</label>
+                    <input
+                        type='text'
+                        id="updatedDesignation"
+                        value={updatedDesignation}
+                        onChange={(e) => setUpdatedDesignation(e.target.value)}
+                    />
+                    <label htmlFor="updatedSalary">Salary:</label>
+                    <input
+                        type='text'
+                        id="updatedSalary"
+                        value={updatedSalary}
+                        onChange={(e) => setUpdatedSalary(e.target.value)}
+                    />
+                    
+                    <label htmlFor="updatedAddress">Address:</label>
+                    <input
+                        type='text'
+                        id="updatedAddress"
+                        value={updatedAddress}
+                        onChange={(e) => setUpdatedAddress(e.target.value)}
+                    />
+
+                    <label htmlFor="updatedDOB">Date of Birth</label>
+                    <input
+                        type='date'
+                        id="updatedDOB"
+                        value={updatedDob}
+                        onChange={(e) => setUpdatedDob(e.target.value)}
+                    />
+
+                    </div>
+                    <div className='options'>
+                        <button style={{backgroundColor: "green"}} onClick={() => handleCreate()}>Create</button>
+                        <button style={{backgroundColor: "red"}} onClick={() => handleCancel()}>Cancel</button>
+                    </div>
+                </div>
+                </div>
+            )}
+            <div className='Table'>
+                <table className='Table'>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Age</th>
+                            <th>Dept</th>
+                            <th>Designation</th>
+                            <th>Salary</th>
+                            <th>Address</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {(userData.length>0) ?
+                    userData.map((user) => (
+                            <tr key={user.id}>
+                                <td className='tdcenter'>{user.id}</td>
+                                <td>{user.emp_name}</td>
+                                <td className='tdcenter'>{user.age}</td>
+                                <td className='tdcenter'>{user.dept}</td>
+                                <td className='tdcenter'>{user.des}</td>
+                                <td className='tdcenter'>{user.sal}</td>
+                                <td>{user.address}</td>
+                                <td style={{justifyContent: "space-between"}}>
+                                    <button style={{backgroundColor: "green"}} onClick={()=>handleUpdate(user)}>Edit</button>
+                                    <span> </span>
+                                    <button style={{backgroundColor: "red"}} onClick={()=>{setDelEmpId(user.id); handleDelete()}}>DELETE</button>
+                                </td>
+                            </tr> 
+                            ))
+                            :   
+                            <tr>
+                                <td>No Data Found!!!</td>
+                            </tr>
+                        }
+                            </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
